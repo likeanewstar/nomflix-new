34ad0df376b0a480844dbf394db501738fcd4f10
likeanewstar <kangsaebyeol@gmail.com>
2020-07-09 21:29:25 +0900

    #5.5 Detail Container part One

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -8,6 +8,16 @@ export default class extends React.Component {
         loading: true
     }
 
+    async componentDidMount() {
+        const {match: {params: {id}}, history: {push}} = this.props;
+        const parseId = parseInt(id)
+        if(isNaN(parseId)){
+            return push("/")
+            // 여기에 return을 써주지 않으면 함수가 종료되지 않아서 조건에 만족하더라도 아랫줄의 console.log까지 실행됨
+        }
+        console.log('wow!')
+    }
+
     render() {
         const { result, error, loading } = this.state;
         return (
