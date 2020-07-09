c702f326ebb5e7d4e80e147d807aff30eb990cfa
sonsuimy <ssigithub@gmail.com>
2020-07-09 20:35:23 +0900

    #5.3 TV Container

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "../../api";
 
 export default class extends React.Component {
     state = {
@@ -10,8 +11,26 @@ export default class extends React.Component {
         loading: true
     }
 
+async componentDidMount () {
+    try {
+        const {data: {result: topRated}} = await tvApi.topRated();
+        const {data: {result: popular}} = await tvApi.popular();
+        const {data: {result: airingToday}} = await tvApi.airingToday();
+        this.setState({topRated, popular, airingToday})
+    } catch {
+        this.setState({
+            error: "Can't find TV information."
+        })
+    } finally {
+        this.setState({
+            loading: false
+        })
+    }
+}
+
     render() {
         const { topRated, popular, airingToday, error, loading } = this.state;
+        console.log(this.state)
         return (
             <TVPresenter 
                 topRated={topRated}
